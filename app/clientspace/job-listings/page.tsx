6948085d778b6adcd172e05468c4e22576fe424b
leenock <leenock176@gmail.com>
2025-05-29 14:07:20 +0300

    CLIENT AREA UPT

---
+++ app/clientspace/job-listings/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function JobListingsPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">Job Listings</h1>
+      <p className="text-gray-600">This page will display job listings.</p>
+      {/* Additional content can be added here */}
+    </div>
+  );
+}
